4bcb755aa887619f1e4559a6867b571751b5626e
Dasha Shcherban <daraserban879@gmail.com>
2025-10-20 18:55:02 +0200

    lab 4 finish

--- lab 4/main.py
+++ lab 4/main.py
@@ -1,15 +1,47 @@
+def find_dublicates(list2):
+    dublicate_list = []
+    print(list2)
+    for i in range(len(list2)):
+        k = list2[i]
+        if k in dublicate_list:
+            continue
+
+        for j in range(i+1, len(list2)):
+            if list2[j] == k:
+                dublicate_list.append(k)
+                break
+    return dublicate_list
+
+def add_elements_with_oddindex(list):
+    second_list = []
+    print(list)
+    i = 0
+    while i < len(list) :
+        if i%2 == 0:
+            print(list[i])
+            second_list.append(list[i])
+        i += 1
+    return second_list
+
 print("-"*15, "Task 1", "-"*15)
 print("Реалізувати одновимірний масив, довжину масива та сам масив користувач має ввести з клавіатури. Виконати над масивом обчислення, вказані  у Вашому варіанті")
 print("Дано одномірний масив, що складається з N дійсних елементів. Масив користувач має ввести з клавіатури. Знайти максимальний від’ємний елемент.")
 
-n = int(input("value of arrays elements:"))
-print(f"Enter {n} array elements:")
+n = int(input("Введіть кількість елементів масиву:"))
+arr = []
+print(f"Введіть {n} дійсних елементів (чисел):")
+for i in range(n):
+    element = float(input(f"Елемент {i + 1}: "))
+    arr.append(element)
 
-arr = [int(input()) for _ in range(n)]
-print("Original array: ", arr)
+print("Початковий масив: ", arr)
+
 negative_elements = [element for element in arr if element < 0]
-print(negative_elements)
-print (min (negative_elements))
+if negative_elements:
+    print(negative_elements)
+    print (max(negative_elements))
+else:
+    print("Немеє відємних елементів")
 
 
 print("-"*15, "Task 2", "-"*15)
@@ -19,44 +51,56 @@ a = [[j-i for j in range(N2)] for i in range(N2)]
 for row in a:
     print(*row)
 
+
 print("-"*15, "Task 3", "-"*15)
 print("Реалізувати функцію, яка виконує операції над списками – задану за варіантом та друк списку на екран. Список користувач має вводити з клавіатури.")
 print("Доповнення списку у кінці елементами списку з парними індексами.")
 
 list_input = input("Введіть елементи через побіл: ")
 list = list_input.split(' ')
-second_list = []
-print(list)
-i = 0
-while i < len(list) :
-    if i%2 == 0:
-        print(list[i])
-        second_list.append(list[i])
-    i += 1
-final_list = list + second_list
-print("Кінцевий список: ".format(final_list))
+funtion_list = add_elements_with_oddindex(list)
+final_list = list + funtion_list
+print("Кінцевий список: ", final_list)
 
 
 print("-"*15, "Task 4", "-"*15)
 print("Реалізувати функцію, яка виконує операції над списками – задану за варіантом та друк списку на екран.")
 print("Формування нового списку, у який входять елементи, що повторюються у попередньому списку.")
+
 list2_input = input("Введіть елементи через побіл: ")
 list2 = list2_input.split(' ')
-second2_list = []
-print(list2)
-i = 1
-while i < len(list2) :
-    if list2[] == list2[i-1]:
-        print(list2[i])
-        second2_list.append(list2[i])
-    i += 1
-final2_list = list2 + second2_list
-print("Кінцевий список: ".format(final2_list))
+repeat_items = find_dublicates(list2)
+print("Введений список:", list2)
+print("Список повторюваних елементів:", repeat_items)
 
 
 print("-"*15, "Task 5", "-"*15)
-print(" Реалізувати функцію, яка виконує операції над множинами – задану за варіантом та друк множини на екран. У випадку, якщо задану варіантом операцію над множиною виконати не можна, перетворіть множину у список, а потім при виведенні на екран результуючий список перетворіть на множину.")
+print("Реалізувати функцію, яка виконує операції над множинами – задану за варіантом та друк множини на екран. У випадку, якщо задану варіантом операцію над множиною виконати не можна, перетворіть множину у список, а потім при виведенні на екран результуючий список перетворіть на множину.")
 print("Маємо три множини x, y, z цифр {8,9,10,11, …, 21,22}. Змінній х присвоїти множину всіх цілих чисел від 8 до 22, змінній у – множину всіх простих чисел з цього діапазону")
 
+def task3_list():
+    z = []
+    x = []
+    y = []
 
+    i = 8
+    while i <= 22:
+        z.append(i)
+        i += 1
+    print("Масив z", z)
+
+    for i in range(len(z)):
+        if z[i] > 0:
+            x.append(z[i])
+
+    print("Масив x", x)
+
+    for i in range(len(z)):
+        if z[i]%2 != 0:
+            if z[i]%3 != 0:
+                y.append(z[i])
+
+    print("Масив y", y)
+
+list_3 = task3_list()
 
